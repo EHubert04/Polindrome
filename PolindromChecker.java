50c43c80e0101821dfba66a0fd3d09da89ec3701
coinstore <75029607+EHubert04@users.noreply.github.com>
2021-12-14 11:21:53 +0100

    for schleife

--- PolindromChecker.java
+++ PolindromChecker.java
@@ -6,11 +6,14 @@ public class PolindromChecker{
     Scanner scan= new Scanner(System.in);
     String polindrom = scan.nextLine();
     Stack stack = new Stack();
+    String reverse = "";
 
     public void check(){
         for(int i = 1;i > polindrom.length(); i++ ){
             stack.push(polindrom.substring(i,i));
-            
+            String reverse = "" + stack.pop();
         }
+       
+        
     }
 }
